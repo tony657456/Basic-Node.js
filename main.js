9ab5891e05a728766f9178c8629c98c2ccb76e93
Your Name <you@example.com>
2022-09-18 15:11:03 +0900

    update / 리다이렉션

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return ` 
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -46,7 +46,10 @@ var app = http.createServer(function(request,response){
         var title = 'Welcome';
         var description = 'Hello, Node.js';
         var list = templateList(filelist);
-        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+        var template = templateHTML(title, list,
+           `<h2>${title}</h2>${description}`,
+           `<a href = "/create">create</a>`
+           );
         response.writeHead(200);
         response.end(template);
       });
@@ -59,7 +62,8 @@ var app = http.createServer(function(request,response){
         fs.readFile(`./data/${queryData.id}`, 'utf8', function(err,description){
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,
+          `<a href = "/create">create</a> <a href="/update1/id=${title}">update</a>`);
           response.writeHead(200);
           response.end(template);
         });
@@ -80,7 +84,7 @@ var app = http.createServer(function(request,response){
             <input type="submit">
           </p>
         </form>
-        `);
+        `, ' ');
         response.writeHead(200);
         response.end(template);
       });
